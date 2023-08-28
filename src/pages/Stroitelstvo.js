edf3250d4e825c193c2c273e2ca7e413c2a6364e
Shod11k <abzalboxodirov99@gmail.com>
2023-08-28 15:51:45 +0300

    fsdfdsf
    sdfsdfsdfsd

---
+++ src/pages/Stroitelstvo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import err from './Group 269.png'
+export default function Stroitelstvo() {
+  return (
+    <div id='hghghghghg'>
+      <div className='error'> 
+      <img src={err} alt=''/>
+      <a href=''  className='back' >Вернуться на главный экран</a>
+      </div>
+    </div>
+  )
+}
