bd96e28ae910ab4ef7d87029faaeaa25e4063faa
Shod11k <abzalboxodirov99@gmail.com>
2023-08-29 16:03:54 +0300

    Err

---
+++ src/js/Error.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import err from './eror.png'
+import '../css/Error.css'
+export default function Stroitelstvo() {
+  return (
+    <div id='hghghghghg'>
+      <div className='error_err'> 
+      <img src={err} alt=''/>
+      <a href=''  className='back' >Вернуться на главный экран</a>
+      </div>
+    </div>
+  )
+}
