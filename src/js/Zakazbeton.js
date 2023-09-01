913c77f0466af9e3fd4f68ad6c5d9d71435f0d97
hydrolifeEco <140441824+hydrolifeEco@users.noreply.github.com>
2023-09-01 09:19:40 +0500

    a
    a

--- src/js/Zakazbeton.js
+++ src/js/Zakazbeton.js
@@ -6,13 +6,83 @@ import { useState } from 'react'
 import IMG1 from "../img/m350 1.png"
 import IMG2 from "../img/cement 1 (1).png"
 import IMG3 from "../img/183973062b952e05464d21c9f08676f9 1.png"
+import kkk from '../img/1548993561_log 1.png'
+import logo from '../img/logo (1).png'
+import logo1 from '../img/logop.png'
+import acardion from '../img/acardion1.png'
+import acardion2 from '../img/acardion2.png'
+import acardion3 from '../img/ph_drop-fill.png'
+import acardion5 from '../img/acardion5.png'
+import acardion6 from '../img/acardion6.png'
 
 import {BsCheckLg} from "react-icons/bs"
 
-
-
-
 export default function Zakazbeton() {
+  function chiqaqol(){
+    var y= document.querySelector(".akardon-pasdia-chiqadi-but").style.display;
+    if(y=="none"){
+    document.querySelector(".akardon-pasdia-chiqadi-but").style.display="block";
+    }else{
+       document.querySelector(".akardon-pasdia-chiqadi-but").style.display="none";
+    }
+
+    var a= document.querySelector(".akardon-text-joyda p").style.color;
+    if(a==="black"){
+    document.querySelector(".akardon-text-joyda p").style.color="#E93333";
+    }else{
+       document.querySelector(".akardon-text-joyda p").style.color="black";
+    }
+    document.querySelector("#zakaz-bt").style.height="2600px";
+}
+function chiqaqol1(){
+    var y= document.querySelector(".alalalalallaalalalal").style.display;
+    if(y==="none"){
+    document.querySelector(".alalalalallaalalalal").style.display="block";
+    }else{
+       document.querySelector(".alalalalallaalalalal").style.display="none";
+    }
+
+    var a= document.querySelector(".akardon-text-joyda1 p").style.color;
+    if(a==="black"){
+    document.querySelector(".akardon-text-joyda1 p").style.color="#E93333";
+    }else{
+       document.querySelector(".akardon-text-joyda1 p").style.color="black";
+    }
+
+    document.querySelector("#zakaz-bt").style.height="2680px";
+
+}
+function chiqaqol2(){
+  var y= document.querySelector(".alalalalallaalalalal1").style.display;
+  if(y==="none"){
+  document.querySelector(".alalalalallaalalalal1").style.display="block";
+  }else{
+     document.querySelector(".alalalalallaalalalal1").style.display="none";
+  }
+
+  var a= document.querySelector(".akardon-text-joyda2 p").style.color;
+  if(a==="black"){
+  document.querySelector(".akardon-text-joyda2 p").style.color="#E93333";
+  }else{
+     document.querySelector(".akardon-text-joyda2 p").style.color="black";
+  }
+}
+function chiqaqol3(){
+  var y= document.querySelector(".axaxaxsaxasxasxasxasxs2").style.display;
+  if(y==="none"){
+  document.querySelector(".axaxaxsaxasxasxasxasxs2").style.display="block";
+  }else{
+     document.querySelector(".axaxaxsaxasxasxasxasxs2").style.display="none";
+  }
+
+  var a= document.querySelector(".akardon-text-joyda3 p").style.color;
+  if(a==="black"){
+  document.querySelector(".akardon-text-joyda3 p").style.color="#E93333";
+  }else{
+     document.querySelector(".akardon-text-joyda3 p").style.color="black";
+  }
+  document.querySelector("#zakaz-bt").style.height="2930px";
+}
   const [zol, setZol] = useState([
     {
       img : IMG1,
@@ -27,6 +97,12 @@ export default function Zakazbeton() {
       name : "Строительный раствор"
     },
   ])
+  const [zag, setZag] = useState(1)
+
+  function global2(){
+    document.querySelector(".zakaz_kvadrat_zagruska").style = "margin-top: 180px !important;"
+    setZag(2) 
+   }
   function yoqil(id){
     for (let i = 0; i < document.querySelectorAll(".card_ptich_dumaloq_ptich").length; i++) {
       if(id === i){
@@ -39,12 +115,11 @@ export default function Zakazbeton() {
       document.querySelectorAll(".zakaz_kvadrat_map_card")[i].style = "border:1px solid #FFCB13;"
      }
       }
-      
     }
   }
   return (
     <div>
-      <div className="zakaz_kvadrat">
+      {zag === 1 ? <div className="zakaz_kvadrat">
         <div className="zakaz_kvadrat_div">
         <span className='zakaz_kvadrat_span'>Что необходимо?</span>
         </div>
@@ -81,11 +156,98 @@ export default function Zakazbeton() {
           </div>
         </div>
         <div className="zakaz_kvadrat_tegi">
-          <button className='zakaz_kvadrat_tegi_but1'><BsArrowLeft/>Назад</button>
-          <button className='zakaz_kvadrat_tegi_but2'>Далее <BsArrowRight/></button>
-
+          <button onClick={()=>window.location="/home"} className='zakaz_kvadrat_tegi_but1'><BsArrowLeft/>Назад</button>
+          <button onClick={()=>{global2()}} className='zakaz_kvadrat_tegi_but2'>Далее <BsArrowRight/></button>
+          </div>
+        </div> : ""}
+      {zag === 2 ? <div className="zakaz_kvadrat">
+        <div className="zakaz_kvadrat_div">
+        <span className='zakaz_kvadrat_span'>Производитель</span>
         </div>
+        <div className="eng-kottade-ulani-ushab-turadi ">
+                        <div className="eng-kottade-scrol eng-kottade-scrol1">
+                        <table className='tab_1'>
+                            <thead>
+                            <tr>
+                            <th className='ana-atble-qildik-p'>Производитель</th>
+                            <th className='ana-atble-qildik-p'>Марка</th>
+                            <th className='ana-atble-qildik-p'>Опции</th>
+                            <th className='ana-atble-qildik-p'>Цена</th>
+                            </tr>
+                            </thead>
+
+          <tbody>
+            <tr className='tr_1'>
+              <th><img src={kkk} alt="" /></th>
+              <th className='pasqismi-narx-joy-zz'>М200</th>
+              <th className='pasqismi-opessaniya'>Наименование опции</th>
+              <th className='pasqismi-narx-joy-zz'>2 000 ₽</th>
+            </tr>
+            <tr className='tr_1'>
+              <th><img src={logo} alt="" /></th>
+              <th className='pasqismi-narx-joy-zz'>М200</th>
+              <th className='pasqismi-opessaniya'>Наименование опции</th>
+              <th className='pasqismi-narx-joy-zz'>2 000 ₽</th>
+            </tr>
+            <tr className='tr_1'>
+              <th><img src={logo1} alt="" /></th>
+              <th className='pasqismi-narx-joy-zz'>М200</th>
+              <th className='pasqismi-opessaniya'>Наименование опции</th>
+              <th className='pasqismi-narx-joy-zz'>2 000 ₽</th>
+            </tr>
+            <tr className='tr_1'>
+              <th><img src={logo1} alt="" /></th>
+              <th className='pasqismi-narx-joy-zz'>М200</th>
+              <th className='pasqismi-opessaniya'>Наименование опции</th>
+              <th className='pasqismi-narx-joy-zz'>2 000 ₽</th>
+            </tr>
+            <tr className='tr_1'>
+              <th><img src={logo1} alt="" /></th>
+              <th className='pasqismi-narx-joy-zz'>М200</th>
+              <th className='pasqismi-opessaniya'>Наименование опции</th>
+              <th className='pasqismi-narx-joy-zz'>2 000 ₽</th>
+            </tr>
+            <tr className='tr_1'>
+              <th><img src={logo1} alt="" /></th>
+              <th className='pasqismi-narx-joy-zz'>М200</th>
+              <th className='pasqismi-opessaniya'>Наименование опции</th>
+              <th className='pasqismi-narx-joy-zz'>2 000 ₽</th>
+            </tr>
+            <tr className='tr_1'>
+              <th><img src={logo1} alt="" /></th>
+              <th className='pasqismi-narx-joy-zz'>М200</th>
+              <th className='pasqismi-opessaniya'>Наименование опции</th>
+              <th className='pasqismi-narx-joy-zz'>2 000 ₽</th>
+            </tr>
+            <tr className='tr_1'>
+              <th><img src={logo1} alt="" /></th>
+              <th className='pasqismi-narx-joy-zz'>М200</th>
+              <th className='pasqismi-opessaniya'>Наименование опции</th>
+              <th className='pasqismi-narx-joy-zz'>2 000 ₽</th>
+            </tr>
+            <tr className='tr_1'>
+              <th><img src={logo1} alt="" /></th>
+              <th className='pasqismi-narx-joy-zz'>М200</th>
+              <th className='pasqismi-opessaniya'>Наименование опции</th>
+              <th className='pasqismi-narx-joy-zz'>2 000 ₽</th>
+            </tr>
+          </tbody>
+        </table>
+            </div>
+                    
+             </div>
+        <div className="zakaz_kvadrat_zagruska">
+          <span>2/9</span>
+          <div className="zakaz_kvadrat_zagruska_ril">
+            <div className="zakaz_kvadrat_zagruska_ril1"></div>
+          </div>
+        </div>
+        <div className="zakaz_kvadrat_tegi">
+          <button onClick={()=>{setZag(1)}} className='zakaz_kvadrat_tegi_but1'><BsArrowLeft/>Назад</button>
+          <button onClick={()=>{setZag(2)}} className='zakaz_kvadrat_tegi_but2'>Далее <BsArrowRight/></button>
+          </div>
+        </div> : ""}
       </div>
-    </div>
+
   )
 }
