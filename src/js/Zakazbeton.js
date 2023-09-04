3246efc67910407d9ea3c6460d3434cd927166ce
hydrolifeEco <140441824+hydrolifeEco@users.noreply.github.com>
2023-09-04 13:02:22 +0500

    a
    a

--- src/js/Zakazbeton.js
+++ src/js/Zakazbeton.js
@@ -19,6 +19,11 @@ import IMG6 from "../img/Group (6).png"
 import IMG7 from "../img/Group (7).png"
 import IMG8 from "../img/Group (8).png"
 import IMG9 from "../img/Group (9).png"
+import IMG10 from "../img/Laptop.png"
+import IMG11 from "../img/Debit Card.png"
+import IMG12 from "../img/Coin In Hand.png"
+import IMG13 from "../img/Money With Arrow.png"
+import IMG14 from "../img/Group (11).png"
 
 import {BsCheckLg} from "react-icons/bs"
 import {FaCalculator} from "react-icons/fa"
@@ -101,6 +106,24 @@ function chiqaqol33(){
       name : "Строительный раствор"
     },
   ])
+  const [zol1, setZol1] = useState([
+    {
+      img : IMG10,
+      name : "Онлайн"
+    },
+    {
+      img : IMG11,
+      name : "Перевод на карту"
+    },
+    {
+      img : IMG12,
+      name : "Наличные"
+    },
+    {
+      img : IMG13,
+      name : "Предоплата"
+    },
+  ])
   const [zag, setZag] = useState(1)
 
   function global2(){
@@ -151,245 +174,18 @@ const [num, setNum] = useState(0)
 const [num1, setNum1] = useState(0)
 const [num2, setNum2] = useState(0)
 const [num3, setNum3] = useState(0)
-
-
-
-
-
-
-
-
-
-
-
-function CalendarControl() {
-    const calendar = new Date();
-    const calendarControl = {
-      localDate: new Date(),
-      prevMonthLastDate: null,
-      calWeekDays: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
-      calMonthName: [
-        "Jan",
-        "Feb",
-        "Mar",
-        "Apr",
-        "May",
-        "Jun",
-        "Jul",
-        "Aug",
-        "Sep",
-        "Oct",
-        "Nov",
-        "Dec"
-      ],
-      daysInMonth: function (month, year) {
-        return new Date(year, month, 0).getDate();
-      },
-      firstDay: function () {
-        return new Date(calendar.getFullYear(), calendar.getMonth(), 1);
-      },
-      lastDay: function () {
-        return new Date(calendar.getFullYear(), calendar.getMonth() + 1, 0);
-      },
-      firstDayNumber: function () {
-        return calendarControl.firstDay().getDay() + 1;
-      },
-      lastDayNumber: function () {
-        return calendarControl.lastDay().getDay() + 1;
-      },
-      getPreviousMonthLastDate: function () {
-        let lastDate = new Date(
-          calendar.getFullYear(),
-          calendar.getMonth(),
-          0
-        ).getDate();
-        return lastDate;
-      },
-      navigateToPreviousMonth: function () {
-        calendar.setMonth(calendar.getMonth() - 1);
-        calendarControl.attachEventsOnNextPrev();
-      },
-      navigateToNextMonth: function () {
-        calendar.setMonth(calendar.getMonth() + 1);
-        calendarControl.attachEventsOnNextPrev();
-      },
-      navigateToCurrentMonth: function () {
-        let currentMonth = calendarControl.localDate.getMonth();
-        let currentYear = calendarControl.localDate.getFullYear();
-        calendar.setMonth(currentMonth);
-        calendar.setYear(currentYear);
-        calendarControl.attachEventsOnNextPrev();
-      },
-      displayYear: function () {
-        let yearLabel = document.querySelector(".calendar .calendar-year-label");
-        yearLabel.innerHTML = calendar.getFullYear();
-      },
-      displayMonth: function () {
-        let monthLabel = document.querySelector(
-          ".calendar .calendar-month-label"
-        );
-        monthLabel.innerHTML = calendarControl.calMonthName[calendar.getMonth()];
-      },
-      selectDate: function (e) {
-        console.log(
-          `${e.target.textContent} ${
-            calendarControl.calMonthName[calendar.getMonth()]
-          } ${calendar.getFullYear()}`
-        );
-      },
-      plotSelectors: function () {
-        document.querySelector(
-          ".calendar"
-        ).innerHTML += `<div class="calendar-inner"><div class="calendar-controls">
-          <div class="calendar-prev"><a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="128" height="128" viewBox="0 0 128 128"><path fill="#666" d="M88.2 3.8L35.8 56.23 28 64l7.8 7.78 52.4 52.4 9.78-7.76L45.58 64l52.4-52.4z"/></svg></a></div>
-          <div class="calendar-year-month">
-          <div class="calendar-month-label"></div>
-          <div>-</div>
-          <div class="calendar-year-label"></div>
-          </div>
-          <div class="calendar-next"><a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="128" height="128" viewBox="0 0 128 128"><path fill="#666" d="M38.8 124.2l52.4-52.42L99 64l-7.77-7.78-52.4-52.4-9.8 7.77L81.44 64 29 116.42z"/></svg></a></div>
-          </div>
-          <div class="calendar-today-date">Today: 
-            ${calendarControl.calWeekDays[calendarControl.localDate.getDay()]}, 
-            ${calendarControl.localDate.getDate()}, 
-            ${calendarControl.calMonthName[calendarControl.localDate.getMonth()]} 
-            ${calendarControl.localDate.getFullYear()}
-          </div>
-          <div class="calendar-body"></div></div>`;
-      },
-      plotDayNames: function () {
-        for (let i = 0; i < calendarControl.calWeekDays.length; i++) {
-          document.querySelector(
-            ".calendar .calendar-body"
-          ).innerHTML += `<div>${calendarControl.calWeekDays[i]}</div>`;
-        }
-      },
-      plotDates: function () {
-        document.querySelector(".calendar .calendar-body").innerHTML = "";
-        calendarControl.plotDayNames();
-        calendarControl.displayMonth();
-        calendarControl.displayYear();
-        let count = 1;
-        let prevDateCount = 0;
-  
-        calendarControl.prevMonthLastDate = calendarControl.getPreviousMonthLastDate();
-        let prevMonthDatesArray = [];
-        let calendarDays = calendarControl.daysInMonth(
-          calendar.getMonth() + 1,
-          calendar.getFullYear()
-        );
-        // dates of current month
-        for (let i = 1; i < calendarDays; i++) {
-          if (i < calendarControl.firstDayNumber()) {
-            prevDateCount += 1;
-            document.querySelector(
-              ".calendar .calendar-body"
-            ).innerHTML += `<div class="prev-dates"></div>`;
-            prevMonthDatesArray.push(calendarControl.prevMonthLastDate--);
-          } else {
-            document.querySelector(
-              ".calendar .calendar-body"
-            ).innerHTML += `<div class="number-item" data-num=${count}><a class="dateNumber" href="#">${count++}</a></div>`;
-          }
-        }
-        //remaining dates after month dates
-        for (let j = 0; j < prevDateCount + 1; j++) {
-          document.querySelector(
-            ".calendar .calendar-body"
-          ).innerHTML += `<div class="number-item" data-num=${count}><a class="dateNumber" href="#">${count++}</a></div>`;
-        }
-        calendarControl.highlightToday();
-        calendarControl.plotPrevMonthDates(prevMonthDatesArray);
-        calendarControl.plotNextMonthDates();
-      },
-      attachEvents: function () {
-        let prevBtn = document.querySelector(".calendar .calendar-prev a");
-        let nextBtn = document.querySelector(".calendar .calendar-next a");
-        let todayDate = document.querySelector(".calendar .calendar-today-date");
-        let dateNumber = document.querySelectorAll(".calendar .dateNumber");
-        prevBtn.addEventListener(
-          "click",
-          calendarControl.navigateToPreviousMonth
-        );
-        nextBtn.addEventListener("click", calendarControl.navigateToNextMonth);
-        todayDate.addEventListener(
-          "click",
-          calendarControl.navigateToCurrentMonth
-        );
-        for (var i = 0; i < dateNumber.length; i++) {
-            dateNumber[i].addEventListener(
-              "click",
-              calendarControl.selectDate,
-              false
-            );
-        }
-      },
-      highlightToday: function () {
-        let currentMonth = calendarControl.localDate.getMonth() + 1;
-        let changedMonth = calendar.getMonth() + 1;
-        let currentYear = calendarControl.localDate.getFullYear();
-        let changedYear = calendar.getFullYear();
-        if (
-          currentYear === changedYear &&
-          currentMonth === changedMonth &&
-          document.querySelectorAll(".number-item")
-        ) {
-          document
-            .querySelectorAll(".number-item")
-            [calendar.getDate() - 1].classList.add("calendar-today");
-        }
-      },
-      plotPrevMonthDates: function(dates){
-        dates.reverse();
-        for(let i=0;i<dates.length;i++) {
-            if(document.querySelectorAll(".prev-dates")) {
-                document.querySelectorAll(".prev-dates")[i].textContent = dates[i];
-            }
-        }
-      },
-      plotNextMonthDates: function(){
-       let childElemCount = document.querySelector('.calendar-body').childElementCount;
-       //7 lines
-       if(childElemCount > 42 ) {
-           let diff = 49 - childElemCount;
-           calendarControl.loopThroughNextDays(diff);
-       }
-
-       //6 lines
-       if(childElemCount > 35 && childElemCount <= 42 ) {
-        let diff = 42 - childElemCount;
-        calendarControl.loopThroughNextDays(42 - childElemCount);
-       }
-
-      },
-      loopThroughNextDays: function(count) {
-        if(count > 0) {
-            for(let i=1;i<=count;i++) {
-                document.querySelector('.calendar-body').innerHTML += `<div class="next-dates">${i}</div>`;
-            }
-        }
-      },
-      attachEventsOnNextPrev: function () {
-        calendarControl.plotDates();
-        calendarControl.attachEvents();
-      },
-      init: function () {
-        calendarControl.plotSelectors();
-        calendarControl.plotDates();
-        calendarControl.attachEvents();
-      }
-    };
-    calendarControl.init();
+function check_ochil(id){
+  for (let i = 0; i < document.querySelectorAll(".oplata_map_card_dumaloq").length; i++) {
+    if(id === i){
+      document.querySelectorAll(".oplata_map_card_dumaloq_chek")[i].style = "display:block"
+      document.querySelectorAll(".oplata_map_card")[i].style = "border:1px solid #FFCB13"
+    }
+    else{
+      document.querySelectorAll(".oplata_map_card_dumaloq_chek")[i].style = "display:none"
+      document.querySelectorAll(".oplata_map_card")[i].style = "border:none"
+    }
   }
-  
-  const calendarControl = new CalendarControl();
-
-
-
-
-
-
-
+}
 
 
   return (
@@ -445,7 +241,7 @@ function CalendarControl() {
             <span>М200</span>
             <span>М400</span>
           </div>
-          <input className='range1' type="range" name="" id="" />
+          <input min={1} max={3} className='range1' type="range" name="" id="" />
         </div>
         <div className="eng-kottade-ulani-ushab-turadi da_boladi">
                         <div className="eng-kottade-scrol eng-kottade-scrol1">
@@ -689,7 +485,7 @@ function CalendarControl() {
               <span>Ограничения по высоте (м)</span><br />
               </div>
               <span className='kuda_span_0'>{num3}</span><br />
-              <input onChange={(e)=>setNum3(e.target.value)} className='range_3' type="range" />
+              <input  min={1} max={150} onChange={(e)=>setNum3(e.target.value)} className='range_3' type="range" />
             </div>
           </div>
         </div>
@@ -739,7 +535,58 @@ function CalendarControl() {
         <div className="zakaz_kvadrat_div">
         <span className='zakaz_kvadrat_span'>Когда?</span>
         </div>
-        <div class="calendar"><div class="calendar-body"></div></div>
+
+        <div className="kogda_div">
+          <div className="kogda_div1">
+            <button className='kogda_div1_but1'>Как можно быстрее</button>
+            <button className='kogda_div1_but2'>Сегодня</button>
+            <button className='kogda_div1_but3'>Предварительный заказ</button>
+            <div className="kogda_div1_check">
+              <input className='checkbox_3' type="checkbox" name="" id="" />
+              <span>Разбить заказ на части</span>
+            </div>
+          </div>
+          <div className="kogda_div2">
+            <div className="kogda_div2_1">
+              <div className="kogda_div2_1_voht"><span>9:00 - 12:00</span></div>
+              <div className="kogda_div2_1_voht"><span>12:00 - 15:00</span></div>
+              <div className="kogda_div2_1_voht"><span>12:00 - 15:00</span></div>
+              <div className="kogda_div2_1_voht"><span>18:00 - 21:00</span></div>
+            </div>
+            <div className="kogda_div2_2">
+             <div className="oplkjnm">
+             <div className="kogda_div2_2_sanasi">
+                <div className="kogda_div2_2_sanasi_vohti">
+                  <span>06.07.23</span>
+                </div>
+                <select className='select_kal'>
+                  <option value="">20м3</option>
+                  <option value="">30м3</option>
+                  <option value="">40м3</option>
+                  <option value="">50м3</option>
+                  <option value="">60м3</option>
+                </select>
+              </div>
+              <div className="kogda_div2_2_sanasi">
+                <div className="kogda_div2_2_sanasi_vohti">
+                  <span>06.07.23</span>
+                </div>
+                <select className='select_kal'>
+                  <option value="">20м3</option>
+                  <option value="">30м3</option>
+                  <option value="">40м3</option>
+                  <option value="">50м3</option>
+                  <option value="">60м3</option>
+                </select>
+              </div>
+             </div>
+              <div className="kogda_div2_2_calendari">
+              <div class="calendar"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+
         <div className="zakaz_kvadrat_zagruska zakaz_kvadrat_zagruska_kop">
           <span>6/9</span>
           <div className="zakaz_kvadrat_zagruska_ril">
@@ -751,7 +598,55 @@ function CalendarControl() {
           <button onClick={()=>{setZag(7)}} className='zakaz_kvadrat_tegi_but2'>Далее <BsArrowRight/></button>
           </div>
         </div> : ""}  
-      </div>
+        {zag === 7 ? <div className="zakaz_kvadrat">
+        <div className="zakaz_kvadrat_div">
+        <span className='zakaz_kvadrat_span'>Способ оплаты</span>
+        </div>
 
+        <div className="oplata_div">
+          <div className="oplata_map">
+          {zol1.map((item,key)=>{
+            return <div key={key} className="oplata_map_card">
+              <div className="oplata_map_card_img">
+                <img src={IMG14} alt="" />
+                <div onClick={()=>{check_ochil(key)}} className="oplata_map_card_dumaloq">
+                  <BsCheckLg className='oplata_map_card_dumaloq_chek'/>
+                </div>
+              </div>
+              <div className="oplata_map_card_img_rasm">
+                <img src={item.img} alt="" />
+              </div>
+              <div className="oplata_map_card_img_span">
+                <span>{item.name}</span>
+              </div>
+            </div>
+          })}
+          </div>
+          <div className="oplata_div_sozi">
+            <div className="oplata_div_sozi111111">
+            <span className='oplata_div_sozispan1'>С какой купюры потребуется сдача?</span><br />
+            </div>
+            <div className="oplata_div_input">
+            <input className='kuda_div1_input' type="text"  placeholder='Поле для ввода'/><br />
+            </div>
+            <span className='oplata_div_sozispan2'>Если сумма заказа <span className='oplata_div_sozispan3'>превышает 150000</span> может потребоваться <span className='oplata_div_sozispan3'>предоплата в 5000 рублей.</span></span>
+          </div>
+
+
+        </div>
+      
+        <div className="zakaz_kvadrat_zagruska zakaz_kvadrat_zagruska_kop">
+          <span>7/9</span>
+          <div className="zakaz_kvadrat_zagruska_ril">
+            <div className="zakaz_kvadrat_zagruska_ril1"></div>
+          </div>
+        </div>
+        <div className="zakaz_kvadrat_tegi">
+          <button onClick={()=>{setZag(6)}} className='zakaz_kvadrat_tegi_but1'><BsArrowLeft/>Назад</button>
+          <button onClick={()=>{setZag(8)}} className='zakaz_kvadrat_tegi_but2'>Далее <BsArrowRight/></button>
+          </div>
+        </div> : ""}   
+      </div>
+   
   )
 }
